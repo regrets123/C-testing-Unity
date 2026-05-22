433354611168aea1889f22705604a32b86a9d17c
Lotus <lotus.saxe@gmail.com>
2026-05-22 09:43:36 +0200

    cleaning up some files.

---
+++ main/main.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+
+void app_main(void)
+{
+    printf("Hello world!\n");
+   
+}
